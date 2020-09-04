b7db5ec6aa2700dded7fce36a99f525209967a5b
Nicholas <69174708+berubenic@users.noreply.github.com>
2020-09-03 20:30:35 -0400

    finsihed fibonacci

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function (member) {
+  member = parseInt(member);
+  if (member <= 0) {
+    return "OOPS";
+  }
 
-}
+  let firstNumber = 1;
+  let secondNumber = 1;
 
-module.exports = fibonacci
+  const sequence = [1, 1];
+
+  for (i = 1; i <= member - 2; i++) {
+    let nextNumber = firstNumber + secondNumber;
+    firstNumber = secondNumber;
+    secondNumber = nextNumber;
+    sequence.push(nextNumber);
+  }
+
+  return sequence[member - 1];
+};
+
+module.exports = fibonacci;
